f55418c59699e0e08dd9a176499d45669d463812
noreply <noreply@yourdomain.com>
2024-02-24 17:37:54 +0800

    v0.1.7 debug

--- features/sendtome/sendtome.go
+++ features/sendtome/sendtome.go
@@ -20,14 +20,13 @@ var syncMap sync.Map
 // OnAudio             = "\aaudio"
 // OnAnimation         = "\aanimation"
 // OnDocument          = "\adocument"
-// OnSticker           = "\asticker"
 // OnVideo             = "\avideo"
 // OnVoice             = "\avoice"
-// OnVideoNote         = "\avideo_note"
 func init() {
 	features.RegisterFeature(tele.OnText, OnPrivateSendToMe)
 	features.RegisterFeature(tele.OnPhoto, OnPrivateSendToMeByPhoto)
 	features.RegisterFeature(tele.OnAudio, OnPrivateSendToMeByAudio)
+	features.RegisterFeature(tele.OnAnimation, OnPrivateSendToMeByAnimation)
 	features.RegisterFeature(tele.OnDocument, OnPrivateSendToMeByDocument)
 	features.RegisterFeature(tele.OnVideo, OnPrivateSendToMeByVideo)
 	features.RegisterFeature(tele.OnVoice, OnPrivateSendToMeByVoice)
@@ -162,12 +161,14 @@ func OnPrivateSendToMeByPhoto(c tele.Context) error {
 			fmt.Println("收到回复消息：", string(jsonText))
 		}
 		replyToText := c.Message().ReplyTo.Text
-		if len(replyToText) == 0 && len(c.Message().ReplyTo.Caption) > 0 {
+		if len(c.Message().ReplyTo.Caption) > 0 {
 			replyToText = c.Message().ReplyTo.Caption
 		}
-		prefixLine, _, isFound := strings.Cut(replyToText, "\n")
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
 		if !isFound {
-			return c.Reply("回复消息格式异常(OnPhoto)(prefixLine): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
 		}
 		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
 		if !isFound {
@@ -266,12 +267,14 @@ func OnPrivateSendToMeByAudio(c tele.Context) error {
 			fmt.Println("收到回复消息：", string(jsonText))
 		}
 		replyToText := c.Message().ReplyTo.Text
-		if len(replyToText) == 0 && len(c.Message().ReplyTo.Caption) > 0 {
+		if len(c.Message().ReplyTo.Caption) > 0 {
 			replyToText = c.Message().ReplyTo.Caption
 		}
-		prefixLine, _, isFound := strings.Cut(replyToText, "\n")
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
 		if !isFound {
-			return c.Reply("回复消息格式异常(OnAudio)(prefixLine): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
 		}
 		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
 		if !isFound {
@@ -338,6 +341,111 @@ func OnPrivateSendToMeByAudio(c tele.Context) error {
 	return c.Reply(what)
 }
 
+// OnAnimation
+func OnPrivateSendToMeByAnimation(c tele.Context) error {
+	if c.Message().FromChannel() || c.Message().FromGroup() {
+		return nil
+	}
+	if !c.Message().Private() && !c.Message().IsReply() {
+		return nil
+	}
+	msgId := ""
+	if len(c.Message().AlbumID) > 0 {
+		msgId = fmt.Sprintf("%d_%s", c.Message().Chat.ID, c.Message().AlbumID)
+	} else { // c.Message().ID > 0
+		msgId = msgId + fmt.Sprintf("%d_%d", c.Message().Chat.ID, c.Message().ID)
+	}
+	if _, ok := syncMap.LoadOrStore(msgId, ""); ok {
+		return nil
+	}
+
+	adminID := os.Getenv("SENDTOME_ID")
+	if len(adminID) == 0 {
+		return nil
+	}
+	senderID := fmt.Sprintf("%d", c.Message().Sender.ID)
+	// 管理员回复信息
+	if c.Message().IsReply() && strings.EqualFold(senderID, adminID) {
+		if jsonText, err := json.Marshal(c.Message()); err != nil {
+			fmt.Println("收到回复消息(err)：", c.Message())
+		} else {
+			fmt.Println("收到回复消息：", string(jsonText))
+		}
+		replyToText := c.Message().ReplyTo.Text
+		if len(c.Message().ReplyTo.Caption) > 0 {
+			replyToText = c.Message().ReplyTo.Caption
+		}
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
+		if !isFound {
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
+		}
+		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
+		if !isFound {
+			return c.Reply("回复消息格式异常(OnAudio)(sendToID): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+		}
+
+		reciverId, err := strconv.ParseInt(sendToID, 10, 64)
+		if err != nil {
+			fmt.Print("回复消息格式异常(OnAudio): 待回复id %s\n%+v", sendToID, c.Message().ReplyTo)
+		}
+		reciver := &tele.User{
+			ID: reciverId,
+		}
+
+		if c.Message().Animation != nil {
+			newMsg := c.Message().Animation
+			if _, err := c.Bot().Send(reciver, newMsg); err != nil {
+				return c.Reply("⚠️回复内容转投失败，请重试。" + err.Error())
+			}
+			return c.Reply("✅回复内容转投成功。")
+		}
+		return c.Reply("⚠️回复内容转投失败，请重试。" + fmt.Sprintf("获取图片信息失败: %+v", c.Message()))
+	}
+
+	// 收到私聊消息
+	if c.Message().Private() && !strings.EqualFold(senderID, adminID) {
+		if jsonText, err := json.Marshal(c.Message()); err != nil {
+			fmt.Println("收到私聊消息(err)：", c.Message())
+		} else {
+			fmt.Println("收到私聊消息：", string(jsonText))
+		}
+
+		reciverId, err := strconv.ParseInt(adminID, 10, 64)
+		if err != nil {
+			fmt.Println("设置有误：环境变量(SENDTOME_ID)：", adminID)
+		}
+		reciver := &tele.User{
+			ID: reciverId,
+		}
+		if c.Message().Animation != nil {
+			newMsg := c.Message().Animation
+			msgText := fmt.Sprintf("@%s #id%d\n%s",
+				c.Message().Sender.Username,
+				c.Message().Sender.ID,
+				c.Message().Caption)
+			newMsg.Caption = msgText
+
+			if _, err := c.Bot().Send(reciver, newMsg); err != nil {
+				return err
+				// return c.Reply("感谢！⚠️私聊内容转投失败。" + err.Error())
+			}
+			return nil
+			// return c.Reply("感谢！✅私聊内容转投成功。")
+		}
+		return fmt.Errorf("获取图片信息失败")
+	}
+
+	what := fmt.Sprintf("@%s #id%d\n%s %s\nHi,Admin! 别逗了，宝！\n%s",
+		c.Message().Sender.Username,
+		c.Message().Sender.ID,
+		c.Message().Sender.FirstName,
+		c.Message().Sender.LastName,
+		c.Message().Caption)
+	return c.Reply(what)
+}
+
 // OnDocument
 func OnPrivateSendToMeByDocument(c tele.Context) error {
 	if c.Message().FromChannel() || c.Message().FromGroup() {
@@ -369,12 +477,14 @@ func OnPrivateSendToMeByDocument(c tele.Context) error {
 			fmt.Println("收到回复消息：", string(jsonText))
 		}
 		replyToText := c.Message().ReplyTo.Text
-		if len(replyToText) == 0 && len(c.Message().ReplyTo.Caption) > 0 {
+		if len(c.Message().ReplyTo.Caption) > 0 {
 			replyToText = c.Message().ReplyTo.Caption
 		}
-		prefixLine, _, isFound := strings.Cut(replyToText, "\n")
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
 		if !isFound {
-			return c.Reply("回复消息格式异常(OnDocument)(prefixLine): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
 		}
 		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
 		if !isFound {
@@ -472,12 +582,14 @@ func OnPrivateSendToMeByVideo(c tele.Context) error {
 			fmt.Println("收到回复消息：", string(jsonText))
 		}
 		replyToText := c.Message().ReplyTo.Text
-		if len(replyToText) == 0 && len(c.Message().ReplyTo.Caption) > 0 {
+		if len(c.Message().ReplyTo.Caption) > 0 {
 			replyToText = c.Message().ReplyTo.Caption
 		}
-		prefixLine, _, isFound := strings.Cut(replyToText, "\n")
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
 		if !isFound {
-			return c.Reply("回复消息格式异常(OnVideo)(prefixLine): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
 		}
 		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
 		if !isFound {
@@ -575,12 +687,14 @@ func OnPrivateSendToMeByVoice(c tele.Context) error {
 			fmt.Println("收到回复消息：", string(jsonText))
 		}
 		replyToText := c.Message().ReplyTo.Text
-		if len(replyToText) == 0 && len(c.Message().ReplyTo.Caption) > 0 {
+		if len(c.Message().ReplyTo.Caption) > 0 {
 			replyToText = c.Message().ReplyTo.Caption
 		}
-		prefixLine, _, isFound := strings.Cut(replyToText, "\n")
+		prefixLine, _, isFound := strings.Cut(replyToText+"\n", "\n")
 		if !isFound {
-			return c.Reply("回复消息格式异常(OnVoice)(prefixLine): " + fmt.Sprintf("%+v", c.Message().ReplyTo))
+			return c.Reply(
+				fmt.Sprintf("回复消息格式异常(OnText)(prefixLine: %s): %+v", replyToText, c.Message().ReplyTo),
+			)
 		}
 		_, sendToID, isFound := strings.Cut(prefixLine, "#id")
 		if !isFound {
